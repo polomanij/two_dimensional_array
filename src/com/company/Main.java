6fc893c5907ec0e01cec7636d93ffbf19ac8ed75
polomanij <polomanijmail@gmail.com>
2016-10-03 04:16:07 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int arrayAmount = 3;
+        int maxArrayNumber = 100;
+	    int[][][] array = new int[arrayAmount][arrayAmount][arrayAmount];
+
+        for (int i = 0; i <arrayAmount; i++) {
+            for (int j = 0; j < arrayAmount; j++) {
+                for (int k = 0; k < arrayAmount; k++) {
+                    int randomNumber = random.nextInt(maxArrayNumber);
+
+                    array[i][j][k] = randomNumber;
+                }
+            }
+        }
+
+        for (int i = 0; i <arrayAmount; i++) {
+            for (int j = 0; j < arrayAmount; j++) {
+                for (int k = 0; k < arrayAmount; k++) {
+                    System.out.println( array[i][j][k] );
+                }
+            }
+        }
+
+    }
+}
